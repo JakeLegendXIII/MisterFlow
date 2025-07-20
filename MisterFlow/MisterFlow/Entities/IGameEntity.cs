2874d1e813bd85cd312b7fa86de878f551684bb6
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-19 23:58:20 -0400

    Add IGameEntity base

---
+++ MisterFlow/MisterFlow/Entities/IGameEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MisterFlow.Entities;
+
+internal interface IGameEntity
+{
+	void Update(GameTime gameTime);
+	void Draw(SpriteBatch spriteBatch);
+}
+
