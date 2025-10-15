df5739197fd46da2b40630e567df66ab47b26162
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-10-14 21:20:49 -0400

    Add GridManager to start

---
+++ MisterFlow/MisterFlow/Entities/GridManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MisterFlow.Entities;
+
+internal class GridManager : IGameEntity
+{
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		
+	}
+}
