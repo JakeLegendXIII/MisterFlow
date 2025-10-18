8785568e900956d21190d177a4de6fe08f2e56d6
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-10-18 15:16:49 -0400

    Add rough Level Data structure

---
+++ MisterFlow/MisterFlow/Data/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MisterFlow.Data;
+
+internal class Level
+{
+	public int Width { get; init; }
+	public int Height { get; init; }
+	public int Size { get; init; }
+
+	public List<TileData> Tiles { get; init; } = new List<TileData>();
+
+	public Level(int width, int height, int size)
+	{
+		Width = width;
+		Height = height;
+		Size = size;
+	}
+}
+
+internal class TileData
+{
+	public int ImageIndex { get; init; }
+	public float Rotation { get; init; }
+}
