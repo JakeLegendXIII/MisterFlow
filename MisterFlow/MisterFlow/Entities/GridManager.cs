0fab4c92f4e5b0cd502a4fa9210f1335dc0145b7
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-10-18 14:52:43 -0400

    Start setting up Grid

--- MisterFlow/MisterFlow/Entities/GridManager.cs
+++ MisterFlow/MisterFlow/Entities/GridManager.cs
@@ -5,6 +5,15 @@ namespace MisterFlow.Entities;
 
 internal class GridManager : IGameEntity
 {
+	private int _gridWidth = 8;
+	private int _gridHeight = 8;
+	private int _cellSize = 64;
+
+	public GridManager()
+	{
+
+	}
+
 	public void Draw(SpriteBatch spriteBatch)
 	{
 		
@@ -13,5 +22,22 @@ internal class GridManager : IGameEntity
 	public void Update(GameTime gameTime)
 	{
 		
+	}
+
+
+	private void InitializeGrid()
+	{
+		for (int x = 0; x < _gridWidth; x++)
+		{
+			for (int y = 0; y < _gridHeight; y++)
+			{				
+				Vector2 position = new Vector2(x * _cellSize, y * _cellSize);
+				Point gridPosition = new Point(x, y);
+
+				//GridSquare gridSquare = new GridSquare(GetObstacleData(x, y), position, _cellSize, gridPosition,
+				//	IsWalkablePoint(x, y), IsWater(x, y), IsWinPosition(x, y), IsDestructable(x, y));
+				//_gridSquares.Add(gridSquare);
+			}
+		}
 	}
 }
