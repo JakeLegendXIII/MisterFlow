5a67a948404b0889e5c1326f825ca750d232733a
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-10-18 22:21:51 -0400

    Handle rotations when clicking

--- MisterFlow/MisterFlow/Entities/GridSquare.cs
+++ MisterFlow/MisterFlow/Entities/GridSquare.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using MisterFlow.Graphics;
 using MisterFlow.Input;
 using MisterFlow.Library;
+using System;
 
 namespace MisterFlow.Entities;
 internal class GridSquare : IGameEntity
@@ -10,38 +11,67 @@ internal class GridSquare : IGameEntity
 	private Vector2 _position;
 	private Point _gridPosition;
 	private Rectangle _drawBox;
+	private Rectangle _positionRectangle;
 
 	private Texture2D _texture;
 
 	private bool _isStart;
 	private bool _isEnd;
+	private float _rotation; // Rotation in radians
+	private Vector2 _origin; // Origin point for rotation
 
 	public GridSquare(Vector2 position, Point gridPosition, Rectangle drawBox, bool isStart, bool isEnd)
 	{
 		_position = position;
+		_positionRectangle = new Rectangle((int)position.X, (int)position.Y, drawBox.Width, drawBox.Height);
 		_gridPosition = gridPosition;
 		_drawBox = drawBox;
 
 		_texture = AssetManager.Octagons;
 		_isStart = isStart;
 		_isEnd = isEnd;
+		_rotation = 0f;
+		// Set origin to center of the sprite for rotation
+		_origin = new Vector2(drawBox.Width / 2f, drawBox.Height / 2f);
 	}
 
 	public void Draw(SpriteBatch spriteBatch)
 	{
-		//Vector2 endTurnIconMousePosition = InputManager.GetTransformedMousePosition(_endTurnRectangle.X, _endTurnRectangle.Y);
+		Vector2 squareMousePosition = InputManager.GetTransformedMousePosition(_positionRectangle.X, _positionRectangle.Y);
 
-		//if (endTurnIconMousePosition.X >= 0 && endTurnIconMousePosition.X <= 128 &&
-		//		endTurnIconMousePosition.Y >= 0 && endTurnIconMousePosition.Y <= 128)
-		//{
-		//	RectangleSprite.DrawRectangle(spriteBatch, _endTurnRectangle, Color.White, 5);
-		//}
+		if (squareMousePosition.X >= 0 && squareMousePosition.X <= 64 &&
+				squareMousePosition.Y >= 0 && squareMousePosition.Y <= 64)
+		{
+			RectangleSprite.DrawRectangle(spriteBatch, _positionRectangle, Color.White, 5);
+		}
 
-		spriteBatch.Draw(_texture, _position, _drawBox, Color.White);
+		// Draw with rotation - adjust position to account for origin offset
+		Vector2 drawPosition = _position + _origin;
+		spriteBatch.Draw(_texture, drawPosition, _drawBox, Color.White, _rotation, _origin, 1f, SpriteEffects.None, 0f);
 	}
 
 	public void Update(GameTime gameTime)
 	{
-		
+		// Check if mouse is over this square and left button is clicked
+		if (!_isStart && !_isEnd)
+		{
+			Vector2 squareMousePosition = InputManager.GetTransformedMousePosition(_positionRectangle.X, _positionRectangle.Y);
+
+			if (squareMousePosition.X >= 0 && squareMousePosition.X <= 64 &&
+				squareMousePosition.Y >= 0 && squareMousePosition.Y <= 64)
+			{
+				if (InputManager.IsLeftMouseButtonDown())
+				{
+					// Rotate 90 degrees clockwise (PI/2 radians)
+					_rotation += MathHelper.PiOver2;
+					
+					// Keep rotation within 0 to 2*PI range (optional, for cleaner values)
+					if (_rotation >= MathHelper.TwoPi)
+					{
+						_rotation -= MathHelper.TwoPi;
+					}
+				}
+			}
+		}
 	}
 }
