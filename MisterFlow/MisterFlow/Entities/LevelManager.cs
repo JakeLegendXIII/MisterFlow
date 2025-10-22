e78037c4d57c0069f7fe03515eacb88273ddf66b
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-10-22 10:44:53 -0400

    Refactor

--- MisterFlow/MisterFlow/Entities/LevelManager.cs
+++ MisterFlow/MisterFlow/Entities/LevelManager.cs
@@ -1,20 +1,221 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MisterFlow.Data;
+using System.Collections.Generic;
 
 namespace MisterFlow.Entities;
 
 internal class LevelManager : IGameEntity
 {
+    private List<Level> _levels;
+    private GridManager _gridManager;
 
-
+    public LevelManager()
+    {
+        _levels = CreateLevels();
+        _gridManager = new GridManager(_levels[0]);
+    }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        
+        _gridManager.Draw(spriteBatch);
     }
 
     public void Update(GameTime gameTime)
     {
-        
+        _gridManager.Update(gameTime);
+    }
+
+    private List<Level> CreateLevels()
+    {
+        List<Level> levels = new List<Level>();
+
+        Level level = new Level(64, 64, 4);
+
+        // Setup Tiles - 4x4 grid = 16 tiles
+
+        // Row 1
+        TileData tile1 = new TileData
+        {
+            ImageIndex = 2,
+            StartRotation = 270f,
+            SolvedRotation = 270f,
+            IsEndTile = false,
+            IsStartTile = true,
+            GridPosition = new Point(0, 0)
+        };
+        level.Tiles.Add(tile1);
+
+        TileData tile2 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 90f,
+            SolvedRotation = 0f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(0, 1)
+        };
+        level.Tiles.Add(tile2);
+
+        TileData tile3 = new TileData
+        {
+            ImageIndex = 1,
+            StartRotation = 180f,
+            SolvedRotation = 90f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(0, 2)
+        };
+        level.Tiles.Add(tile3);
+
+        TileData tile4 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 270f,
+            SolvedRotation = 180f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(0, 3)
+        };
+        level.Tiles.Add(tile4);
+
+        // Row 2
+        TileData tile5 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 0f,
+            SolvedRotation = 270f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(1, 0)
+        };
+        level.Tiles.Add(tile5);
+
+        TileData tile6 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 90f,
+            SolvedRotation = 0f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(1, 1)
+        };
+        level.Tiles.Add(tile6);
+
+        TileData tile7 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 180f,
+            SolvedRotation = 90f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(1, 2)
+        };
+        level.Tiles.Add(tile7);
+
+        TileData tile8 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 270f,
+            SolvedRotation = 180f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(1, 3)
+        };
+        level.Tiles.Add(tile8);
+
+        // Row 3
+        TileData tile9 = new TileData
+        {
+            ImageIndex = 1,
+            StartRotation = 90f,
+            SolvedRotation = 270f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(2, 0)
+        };
+        level.Tiles.Add(tile9);
+
+        TileData tile10 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 90f,
+            SolvedRotation = 0f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(2, 1)
+        };
+        level.Tiles.Add(tile10);
+
+        TileData tile11 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 180f,
+            SolvedRotation = 90f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(2, 2)
+        };
+        level.Tiles.Add(tile11);
+
+        TileData tile12 = new TileData
+        {
+            ImageIndex = 1,
+            StartRotation = 270f,
+            SolvedRotation = 180f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(2, 3)
+        };
+        level.Tiles.Add(tile12);
+
+        // Row 4
+        TileData tile13 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 0f,
+            SolvedRotation = 270f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(3, 0)
+        };
+        level.Tiles.Add(tile13);
+
+        TileData tile14 = new TileData
+        {
+            ImageIndex = 3,
+            StartRotation = 90f,
+            SolvedRotation = 0f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(3, 1)
+        };
+        level.Tiles.Add(tile14);
+
+        TileData tile15 = new TileData
+        {
+            ImageIndex = 1,
+            StartRotation = 180f,
+            SolvedRotation = 90f,
+            IsEndTile = false,
+            IsStartTile = false,
+            GridPosition = new Point(3, 2)
+        };
+        level.Tiles.Add(tile15);
+
+        TileData tile16 = new TileData
+        {
+            ImageIndex = 2,
+            StartRotation = 0f,
+            SolvedRotation = 0f,
+            IsEndTile = true,
+            IsStartTile = false,
+            GridPosition = new Point(3, 3)
+        };
+        level.Tiles.Add(tile16);
+
+        levels.Add(level);
+
+        return levels;
     }
 }
--- MisterFlow/MisterFlow/MainGame.cs
+++ MisterFlow/MisterFlow/MainGame.cs
@@ -26,7 +26,7 @@ namespace MisterFlow
 		int _height = 0;
 
 		private LevelManager _levelManager;
-        private GridManager _gridManager;
+
 
 		public MainGame()
 		{
@@ -59,10 +59,7 @@ namespace MisterFlow
 
 			AssetManager.Load(Content);
 
-			Level level = CreateLevel();
-
 			_levelManager = new LevelManager();
-            _gridManager = new GridManager(level);
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -73,7 +70,6 @@ namespace MisterFlow
 			InputManager.Update(_renderDestination, _scale);
 
 			_levelManager.Update(gameTime);
-            _gridManager.Update(gameTime);
 
 			base.Update(gameTime);
 		}
@@ -89,7 +85,6 @@ namespace MisterFlow
 
 			_spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 			
-            _gridManager.Draw(_spriteBatch);
             _levelManager.Draw(_spriteBatch);
 
             _spriteBatch.End();
@@ -101,196 +96,7 @@ namespace MisterFlow
 			_spriteBatch.End();
 
 			base.Draw(gameTime);
-		}
-
-		private Level CreateLevel()
-		{
-			Level level = new Level(64, 64, 4);
-
-			// Setup Tiles - 4x4 grid = 16 tiles
-			
-			// Row 1
-			TileData tile1 = new TileData
-			{
-				ImageIndex = 2,
-				StartRotation = 270f,
-				SolvedRotation = 270f,
-				IsEndTile = false,
-				IsStartTile = true,
-				GridPosition = new Point(0,0)
-			};
-			level.Tiles.Add(tile1);
-
-			TileData tile2 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 90f,
-				SolvedRotation = 0f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition= new Point(0,1)
-			};
-			level.Tiles.Add(tile2);
-
-			TileData tile3 = new TileData
-			{
-				ImageIndex = 1,
-				StartRotation = 180f,
-				SolvedRotation = 90f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(0,2)
-			};
-			level.Tiles.Add(tile3);
-
-			TileData tile4 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 270f,
-				SolvedRotation = 180f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(0,3)
-			};
-			level.Tiles.Add(tile4);
-
-			// Row 2
-			TileData tile5 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 0f,
-				SolvedRotation = 270f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(1,0)
-			};
-			level.Tiles.Add(tile5);
-
-			TileData tile6 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 90f,
-				SolvedRotation = 0f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition= new Point(1,1)
-			};
-			level.Tiles.Add(tile6);
-
-			TileData tile7 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 180f,
-				SolvedRotation = 90f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(1,2)
-			};
-			level.Tiles.Add(tile7);
-
-			TileData tile8 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 270f,
-				SolvedRotation = 180f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(1,3)
-			};
-			level.Tiles.Add(tile8);
-
-			// Row 3
-			TileData tile9 = new TileData
-			{
-				ImageIndex = 1,
-				StartRotation = 90f,
-				SolvedRotation = 270f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(2,0)
-			};
-			level.Tiles.Add(tile9);
-
-			TileData tile10 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 90f,
-				SolvedRotation = 0f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(2,1)
-			};
-			level.Tiles.Add(tile10);
-
-			TileData tile11 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 180f,
-				SolvedRotation = 90f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(2,2)
-			};
-			level.Tiles.Add(tile11);
-
-			TileData tile12 = new TileData
-			{
-				ImageIndex = 1,
-				StartRotation = 270f,
-				SolvedRotation = 180f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(2,3)
-			};
-			level.Tiles.Add(tile12);
-
-			// Row 4
-			TileData tile13 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 0f,
-				SolvedRotation = 270f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(3,0)
-			};
-			level.Tiles.Add(tile13);
-
-			TileData tile14 = new TileData
-			{
-				ImageIndex = 3,
-				StartRotation = 90f,
-				SolvedRotation = 0f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(3,1)
-			};
-			level.Tiles.Add(tile14);
-
-			TileData tile15 = new TileData
-			{
-				ImageIndex = 1,
-				StartRotation = 180f,
-				SolvedRotation = 90f,
-				IsEndTile = false,
-				IsStartTile = false,
-				GridPosition = new Point(3,2)
-			};
-			level.Tiles.Add(tile15);
-
-			TileData tile16 = new TileData
-			{
-				ImageIndex = 2,
-				StartRotation = 0f,
-				SolvedRotation = 0f,
-				IsEndTile = true,
-				IsStartTile = false,
-				GridPosition = new Point(3,3)
-			};
-			level.Tiles.Add(tile16);
-
-			return level;
-		}
+		}		
 
 		private void OnClientSizeChanged(object sender, EventArgs e)
 		{
