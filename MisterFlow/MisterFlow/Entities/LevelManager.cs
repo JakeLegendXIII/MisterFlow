6804d3b1ea72fb9cf8e80e00fe0af23426fd3a1d
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-10-22 09:47:50 -0400

    Start adding LevelManager

---
+++ MisterFlow/MisterFlow/Entities/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MisterFlow.Entities;
+
+internal class LevelManager : IGameEntity
+{
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        
+    }
+}
